8f77459ae951788cf27fbfa860c3627f3db64b53
Andy-Koo <DarikRunech@gmail.com>
2023-06-20 17:23:57 +0300

    Task1F5

--- HomeTasks1/HomeTasks1/src/Task1.java
+++ HomeTasks1/HomeTasks1/src/Task1.java
@@ -1,3 +1,18 @@
+// 1) Вычислить суммy чисел от 1 до n
+
+import java.util.Scanner;
+
 public class Task1 {
-    
+   public static void main(String[] args) {
+      Scanner input = new Scanner(System.in);
+      System.out.print("Введите число n: ");
+
+      int n = input.nextInt();
+      int sum = 0;
+
+      for (int i = 1; i <= n; i++) {
+         sum += i;
+      }
+      System.out.println("Сумма от 1 до " + n + " равна " + sum);
+   }
 }
