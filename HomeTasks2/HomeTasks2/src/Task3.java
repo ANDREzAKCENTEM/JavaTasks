2b5cda4477e34023de3e91296b3a7d1a24aea2ae
Andy-Koo <DarikRunech@gmail.com>
2023-07-07 19:05:44 +0300

    F4-1

---
+++ HomeTasks2/HomeTasks2/src/Task3.java
@@ -0,0 +1,56 @@
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Task3 {
+    public static void main(String[] args) {
+        File file = new File(System.getProperty("user.dir").concat("/file3.txt"));
+
+        try (BufferedReader bufReader = new BufferedReader(new FileReader(file))) {
+            String line = bufReader.readLine();
+            String[] result = destructureJSON(line);
+
+            for (int i = 0; i < result.length; i++) {
+                System.out.println(result[i]);
+            }
+        } catch (FileNotFoundException fnfe) {
+            System.out.println(fnfe.getMessage());
+        } catch (IOException ioe) {
+            System.out.println(ioe.getMessage());
+        }
+    }
+
+    public static String[] destructureJSON(String JSON) {
+        JSON = JSON.substring(1, JSON.length() - 1);
+        String[] entries = JSON.split(", ");
+        String[] result = new String[entries.length];
+
+        for (int i = 0; i < entries.length; i++) {
+            entries[i] = entries[i].substring(1, entries[i].length() - 1);
+            String[] pairs = entries[i].split(",");
+
+            StringBuilder student = new StringBuilder("Студент ");
+            StringBuilder grade = new StringBuilder(" получил ");
+            StringBuilder subject = new StringBuilder(" по предмету ");
+
+            for (int j = 0; j < pairs.length; j++) {
+                String[] keyValue = pairs[j].split(":");
+
+                if (keyValue[0].equals("\"фамилия\"")) {
+                    student.append(keyValue[1].substring(1, keyValue[1].length() - 1));
+                } else if (keyValue[0].equals("\"оценка\"")) {
+                    grade.append(keyValue[1].substring(1, keyValue[1].length() - 1));
+                } else if (keyValue[0].equals("\"предмет\"")) {
+                    subject.append(keyValue[1].substring(1, keyValue[1].length() - 1));
+                }
+            }
+
+            result[i] = student.append(grade.toString()).append(subject.toString()).append(".").toString();
+        }
+
+        return result;
+    }
+}
